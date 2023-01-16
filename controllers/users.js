cce754bb8b85943289bfcdb02a3e2216d6467a52
RuiqiLiu213 <liuruiqi4638@gmail.com>
2023-01-16 18:39:53 -0500

    upload

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email: email, username: username });
+        // register(user, password, cb) Convenience method to register a new user instance with a given password.
+        const registeredUser = await User.register(user, password);
+        // Passport exposes a login() function on req that can be used to establish a login session.
+        // When the login operation completes, user will be assigned to req.user.
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+        // console.log(req.user, "req.user after login")
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) => {
+    req.logout();
+    // req.session.destroy();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
